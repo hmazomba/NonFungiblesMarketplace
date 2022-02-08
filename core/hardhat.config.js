b1377cea47999b44a43d284b718d77cd4d087032
Hlumisa Mazomba <hmazomba@live.co.za>
2022-02-08 12:14:14 +0200

    Project intialization and config

---
+++ core/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+const projectId = fs.readFileSync(".projectid").toString().trim() || "01234567890123456789"
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [privateKey]
+    },
+    mumbai:{
+      url:"https://polygon-mumbai.infura.io/v3/${projectId}",
+      accounts:[]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
